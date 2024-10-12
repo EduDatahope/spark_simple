1a666348b9df33c80602b921321a72186bd6cf86
egonzo <162932324+EduDatahope@users.noreply.github.com>
2024-10-12 18:26:34 -0500

    v4

---
+++ spark-gcolab/converter_file.py
@@ -0,0 +1,7 @@
+import jupytext
+# read a notebook from a file
+vpath1 ='/Users/edugonzo/Downloads/basic_gcp_colab_sp.ipynb'
+vpath2 ='/Users/edugonzo/Downloads/basic_gcp_colab_sp_format.py'
+
+nb = jupytext.read(vpath1)
+jupytext.write(nb, vpath2, fmt="py:percent")
